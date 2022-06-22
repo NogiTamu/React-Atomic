759c3cc3920af42ecb73a8d12ca761853a1aa9b9
NogiTamu <76116273+NogiTamu@users.noreply.github.com>
2022-06-22 11:25:08 +0900

    atom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
+import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      a<PrimaryButton>テスト</PrimaryButton>
+      <PrimaryButton>テスト</PrimaryButton>
+      <SecondaryButton>うんこ</SecondaryButton>
     </div>
   );
 }
---
+++ src/components/atoms/button/BaseButton.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const BaseButton = styled.button`
+  color: white;
+  border-radius: 999px;
+  border: none;
+  padding: 5px 15px;
+
+  &:hover {
+    cursor: pointer;
+    opacity: 0.8;
+  }
+`;
