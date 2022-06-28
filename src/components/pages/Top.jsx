25d01ca2cd2d060311179c91a559dc909abc64e0
NogiTamu <76116273+NogiTamu@users.noreply.github.com>
2022-06-28 15:20:17 +0900

    ggg s

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,5 +1,17 @@
+import { useContext } from "react";
+import { useHistory } from "react-router-dom";
 import styled from "styled-components";
+import { UserContext } from "../../providers/UserProvider";
+
 export const Top = () => {
+  const history = useHistory();
+  const { setUserInfo } = useContext(UserContext);
+
+  const onClickAdmin = () => {
+    setUserInfo({ isAdmin: true });
+    history.push("/users");
+  };
+
   return (
     <SContainer>
       <h2>Topです</h2>
